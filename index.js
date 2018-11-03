2fdb615f54e607d370d66380704175c2d6ce2dad
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-02 23:11:43 -0300

    Install, configure Embedded JavaScript Templates - EJS

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.set('view engine', 'ejs')
+
 app.get('/', (request, response) => {
-    response.send('Enjoy the silence!')
+    response.render('index')
 })
 
 app.listen(port, (err) => {
