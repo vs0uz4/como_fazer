45dba7859e16c1aabd4d7c1b2ef07e630841ecf0
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-05 13:09:50 -0200

    Reaorder Code, Eliminate Lines and Spaces

--- api.js
+++ api.js
@@ -4,22 +4,20 @@ const baseURL = 'https://como-fazer-add43.firebaseio.com/'
 const echo = async() => {
     const result = await axios.get(baseURL + '/api_echo.json')
     const content = !(result.data) ? 'Enjoy the Silence!' : result.data
-
     return content
 }
 
 const list = async(key) => {
     const result = await axios.get(baseURL + key + '.json')
     const content = !(result.data) ? [] : result.data
-
     const objects = Object
-                        .keys(content)
-                        .map(key => {
-                            return {
-                                id: key,
-                                ...content[key]
-                            }
-                        })
+                    .keys(content)
+                    .map(key => {
+                        return {
+                            id: key,
+                            ...content[key]
+                        }
+                    })
     return objects
 }
 
